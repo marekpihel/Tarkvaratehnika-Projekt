9060342f716e45a6e82f8d04c15ec65a13be8e2b
Dot <Dot@Dots-MacBook-Pro.local>
2016-10-07 11:02:42 +0300

    assets & projectsettings

---
+++ Assets/Scripts/QuitMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuitMenu : MonoBehaviour {
+
+	public Text yes;
+	public Text no;
+
+	// Use this for initialization
+	void Start () {
+		yes = GameObject.Find ("Yes").GetComponent<Text> ();
+		no = GameObject.Find ("No").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// exitMenu -> No
+	public void ExitNoPress() {
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	// exitMenu -> Yes
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
