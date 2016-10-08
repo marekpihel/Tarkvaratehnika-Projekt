82f283515f99c573b6d9539751a3857a26b9b8ff
marek pihel <marek pihel>
2016-10-08 16:59:06 +0300

    Playeri laadimine ning scene

---
+++ Assets/Scripts/PlayerLoading.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLoading : MonoBehaviour {
+    public GameObject player;
+    public GameObject spawnPoint;
+
+    // Use this for initialization
+    void Start() {
+        player.transform.transform.position = spawnPoint.transform.position;
+        Instantiate<GameObject>(player);
+    }
+}
