99c1487bbf5e2f52b1c40a816e061f559814631f
Martin Ojamets <maroja@ttu.ee>
2016-10-09 16:31:57 +0300

    Movement and Fixed spawn Location

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 64);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * 64);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * 64);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * 64);
+        }
+    }
+}
