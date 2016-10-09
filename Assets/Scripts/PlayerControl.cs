b03e0319f4388a8ba9ad239700be9832d43567e7
marek pihel <marek pihel>
2016-10-09 20:09:18 +0300

    Time deltatime lisa playerControlile

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -18,19 +18,19 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.right * 64);
+            transform.Translate(Vector2.right * 64 * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left * 64);
+            transform.Translate(Vector2.left * 64 * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector2.up * 64);
+            transform.Translate(Vector2.up * 64 * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector2.down * 64);
+            transform.Translate(Vector2.down * 64 * Time.deltaTime);
         }
     }
 }
