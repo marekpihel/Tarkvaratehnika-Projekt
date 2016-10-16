9925b3a7e04a3d31490c9f564aad722f387c51eb
Jakob <jakobpin@gmail.com>
2016-10-16 20:00:15 +0300

    added scoreboard numeration

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -8,15 +8,18 @@ using System;
 public class HighScore : MonoBehaviour {
     Text name1;
     Text score1;
+    Text scoreBoardNumbering1;
     int counter = 0;
 
     // Use this for initialization
     void Start () {
         name1 = GameObject.Find("textName").GetComponent<Text>();
         score1 = GameObject.Find("textScore").GetComponent<Text>();
+        scoreBoardNumbering1 = GameObject.Find("scoreboardNumbering").GetComponent<Text>();
 
         foreach (KeyValuePair<string, int> entry in Scoreboard.ReadScoreboard()) {
             if (counter < 10) {
+                scoreBoardNumbering1.text += (counter +1) + " \n";
                 name1.text += entry.Key + " \n";
                 score1.text += Convert.ToString(entry.Value) + " \n";
                 counter++;
