09fbb73f5cf4eec9455099f88db6f9a973ad3991
marek pihel <marek pihel>
2016-10-17 21:51:50 +0300

    Lisasin Ingame UI

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,20 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
 
 public class PlayerControl : MonoBehaviour
 {
+    int playerHealth = 50;
     float speed = 128.0f;                         // Speed of movement
     Rigidbody2D rbody;
     Animator anim;
+    Text healthText;
+    string playerName;
+    double gameTime;
+
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        healthText = GameObject.Find("healthText").GetComponent<Text>();
+        playerName = SetName.getCharacterName();
+        gameTime = GameTime.getPlayedTime();
     }
 
     void Update()
     {
+        gameTime = Mathf.Round((float)GameTime.getPlayedTime());
         Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (movement != Vector2.zero)
@@ -28,6 +39,9 @@ public class PlayerControl : MonoBehaviour
         }
 
         rbody.MovePosition(rbody.position + movement * speed * Time.deltaTime);
+
+        healthText.text = playerHealth.ToString();
+        GameObject.Find("nameText").GetComponent<Text>().text = playerName + " : " + gameTime.ToString();
     }
 
     void OnTriggerEnter2D(Collider2D collisionObject) {
