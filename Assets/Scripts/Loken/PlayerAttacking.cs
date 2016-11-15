eebf9a7a2e315d2884e80c0f9d771d66267b8b87
Martin Ojamets <maroja@ttu.ee>
2016-11-15 22:54:48 +0200

    buggy attack along with blob removal

--- Assets/Scripts/Loken/PlayerAttacking.cs
+++ Assets/Scripts/Loken/PlayerAttacking.cs
@@ -1,27 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerAttacking : MonoBehaviour {
+public class PlayerAttacking : MonoBehaviour
+{
     private float gridSize = PlayerMovement.gridSize;
     private float slashTeleBackWaitTime = 0.45f;
     private Animator animator;
     private BoxCollider2D boxCollider2D;
     private bool isAttacking = false;
     private float playerDirection = 0;
-    private Vector2 playerDirectionInVector2;
+    private Vector2 input;
     private Vector3 startPosition;
     public static int playerHealth = 9;
+    public static int playerDMG = 1;
     public static int currentScore = 0;
     public static bool aliveState = true;
-    
 
-    void Start () {
+
+    void Start()
+    {
         animator = GetComponent<Animator>();
         boxCollider2D = GetComponent<BoxCollider2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (isAlive())
         {
             if (!isAttacking)
@@ -29,27 +33,39 @@ public class PlayerAttacking : MonoBehaviour {
                 playerDirection = animator.GetFloat("direction");
                 if (Input.GetButtonDown("Fire1") && playerDirection != 0)
                 {
+                    input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
                     startPosition = transform.position;
-                    playerDirectionInVector2 = convertDirectionIntoVector2();
                     boxCollider2D.enabled = false;
-                    RaycastHit2D hit = Physics2D.Raycast(startPosition, playerDirectionInVector2, gridSize);
+                    RaycastHit2D hit = Physics2D.Raycast(startPosition, input, gridSize);
                     if (isWithinHittingRange(hit))
                     {
-                        transform.position = new Vector3(startPosition.x + System.Math.Sign(playerDirectionInVector2.x) * gridSize, startPosition.y + System.Math.Sign(playerDirectionInVector2.y) * gridSize, startPosition.z);
-                        Invoke("returnToOrigialPlace", slashTeleBackWaitTime);
+                        transform.position = new Vector3(startPosition.x + System.Math.Sign(input.x) * gridSize, startPosition.y + System.Math.Sign(input.y) * gridSize, startPosition.z);
+                        Invoke("returnToOriginalPlace", slashTeleBackWaitTime);
+                        isAttacking = true;
+                        animateChar();
                     }
-                    isAttacking = true;
-                    animateChar();
+                    else
+                    {
+                        Invoke("setIsAttackFalse", slashTeleBackWaitTime);
+                        isAttacking = true;
+                        animateChar();
+                    }
                 }
             }
         }
         else
         {
-            //Calls level end function in PlayerMovement  
+            //Calls levelEnd function in PlayerMovement  
         }
     }
 
-    private void returnToOrigialPlace()
+    private void setIsAttackFalse()
+    {
+        boxCollider2D.enabled = true;
+        isAttacking = false;
+    }
+
+    private void returnToOriginalPlace()
     {
         transform.position = startPosition;
         boxCollider2D.enabled = true;
@@ -59,12 +75,13 @@ public class PlayerAttacking : MonoBehaviour {
     private bool isWithinHittingRange(RaycastHit2D hit)
     {
         if (hit.collider == null)
-        {
             return false;
-        }
         else if (hit.collider.tag == "Enemy")
         {
-            print("Can Attack");
+            Debug.Log("Attacks Enemy");
+            BlobController blob = hit.collider.gameObject.GetComponent<BlobController>();
+            blob.blobHealth -= playerDMG;
+            Debug.Log(blob.blobHealth);
             return true;
         }
         else
@@ -96,19 +113,5 @@ public class PlayerAttacking : MonoBehaviour {
     public static void addPointsToCurrentScore(int points)
     {
         currentScore += points;
-    }
-
-    private Vector2 convertDirectionIntoVector2()
-    {
-        if (playerDirection == 1)
-            return new Vector2(0, 1);
-        else if (playerDirection == 2)
-            return new Vector2(1, 0);
-        else if (playerDirection == 3)
-            return new Vector2(0, -1);
-        else if (playerDirection == 4)
-            return new Vector2(-1, 0);
-        else
-            return new Vector2(0, 0);
     }
 }
