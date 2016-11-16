e619b6375e090ada6e7e2839fe63b8773f0dfaf6
Marek Pihel <marekpihel@gmail.com>
2016-11-17 00:51:35 +0200

    blob movement

--- Assets/Scripts/Enemy/BlobMovement.cs
+++ Assets/Scripts/Enemy/BlobMovement.cs
@@ -11,13 +11,17 @@ public class BlobMovement : MonoBehaviour {
     private Animator animator;
     private WaitForSeconds waitforseconds;
     private bool haventMoved;
+    private BoxCollider2D boxCollider2D;
+    private bool movementStatus;
 
     // Use this for initialization
     void Start () {
         animator = this.GetComponent<Animator>();
         waitforseconds = new WaitForSeconds(1);
         haventMoved = true;
-	}
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        movementStatus = true;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -28,34 +32,57 @@ public class BlobMovement : MonoBehaviour {
         } else {
             cooldown -= Time.deltaTime;
         }
-        if (haventMoved) {
-            if (direction == 1)
-            {
-                moveUp();
+        if (isMovementDisabled()) {
+            if (haventMoved) {
+                if (direction == 1)
+                {
+                    if (isAllowedToMove(new Vector2(0, 1)))
+                    {
+                        moveUp();
+                    }
+                }
+                else if (direction == 2)
+                {
+                    if (isAllowedToMove(new Vector2(1, 0)))
+                    {
+                        moveRight();
+                    }
+                }
+                else if (direction == 3)
+                {
+                    if (isAllowedToMove(new Vector2(0, -1)))
+                    {
+                        moveDown();
+                    }
+                }
+                else if (direction == 4)
+                {
+                    if (isAllowedToMove(new Vector2(-1, 0)))
+                    {
+                        moveLeft();
+                    }
+                }
+                Invoke("stayStill", 1);
+                haventMoved = false;
             }
-            else if (direction == 2)
-            {
-                moveRight();
-            }
-            else if (direction == 3)
-            {
-                moveDown();
-            }
-            else if (direction == 4)
-            {
-                moveLeft();
-            }
-            Invoke("stayStill", 1);
-            haventMoved = false;
         }
          
+    }
+
+    private bool isMovementDisabled()
+    {
+        return movementStatus;
+    }
+
+    public void disableMovement() {
+        movementStatus = false;
     }
 
     private void stayStill()
     {
         animator.SetBool("isWalking", false);
         animator.SetFloat("value_x", 0);
-        animator.SetFloat("value_y", 0);
+        animator.SetFloat("value_y", 0);       
     }
 
     private void moveLeft()
@@ -63,6 +90,7 @@ public class BlobMovement : MonoBehaviour {
         animator.SetBool("isWalking", true);
         animator.SetFloat("value_x", -1);
         animator.SetFloat("value_y", 0);
+        move(new Vector2(-1, 0));
     }
 
     private void moveDown()
@@ -70,6 +98,7 @@ public class BlobMovement : MonoBehaviour {
         animator.SetBool("isWalking", true);
         animator.SetFloat("value_x", 0);
         animator.SetFloat("value_y", -1);
+        move(new Vector2(0, -1));
     }
 
     private void moveRight()
@@ -77,6 +106,7 @@ public class BlobMovement : MonoBehaviour {
         animator.SetBool("isWalking", true);
         animator.SetFloat("value_x", 1);
         animator.SetFloat("value_y", 0);
+        move(new Vector2(1, 0));
     }
 
     private void moveUp()
@@ -84,5 +114,45 @@ public class BlobMovement : MonoBehaviour {
         animator.SetBool("isWalking", true);
         animator.SetFloat("value_x", 0);
         animator.SetFloat("value_y", 1);
+        move(new Vector2(0, 1));
+    }
+
+    private void move(Vector2 direction) {
+        float time = 0;
+        Vector2 startPosition = transform.position;
+        Vector2 endPosition = new Vector2(startPosition.x + direction.x * gridSize, startPosition.y + direction.y * gridSize);
+        while (time < 1f)
+        {
+            time += Time.deltaTime * (moveSpeed / gridSize);
+            transform.position = Vector2.Lerp(startPosition, endPosition, time);
+        }
+        transform.position = endPosition;
+    }
+
+    private bool isAllowedToMove(Vector2 input)
+    {
+        Vector2 startPosition = transform.position;
+        Vector2 endPosition = new Vector3(startPosition.x, startPosition.y);
+        boxCollider2D.enabled = false;
+        RaycastHit2D hit128 = Physics2D.Raycast(startPosition, input, gridSize * 2);
+        boxCollider2D.enabled = true;
+        if (hit128.collider != null && hit128.collider.name == "Player")
+        {
+            return false;
+        }
+        else
+        {
+            boxCollider2D.enabled = false;
+            RaycastHit2D hit64 = Physics2D.Raycast(startPosition, input, gridSize+13);
+            boxCollider2D.enabled = true;
+            if (hit64.collider == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
